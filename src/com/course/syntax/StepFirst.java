942792c91bb52693a1a7d87cfb4f0ad20a1858ac
Maksym-Semenenko <semenenko.maksym@airslate.com>
2023-04-09 23:20:31 +0300

    StepFirst - variables

---
+++ src/com/course/syntax/StepFirst.java
@@ -0,0 +1,15 @@
+package com.course.syntax;
+
+public class StepFirst {
+    public static void main(String[] args) {
+        int intValue = 21;
+        float floatValue = 21.06f;
+        boolean boolValue = true;
+        char charValue1 = 's';
+        char charValue2 = 'm';
+        if(boolValue == true) {
+            System.out.println(intValue + " " + floatValue + " " + charValue1 + "." + charValue2 + ".");
+        }
+    }
+}
+
