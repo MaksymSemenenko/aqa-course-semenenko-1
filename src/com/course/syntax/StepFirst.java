1c173620f3f18379dd00fcea1d792a0ecf620456
Maksym-Semenenko <semenenko.maksym@airslate.com>
2023-04-10 01:00:05 +0300

    StepFirst - Task

--- src/com/course/syntax/StepFirst.java
+++ src/com/course/syntax/StepFirst.java
@@ -1,7 +1,11 @@
 package com.course.syntax;
 
+import java.util.Scanner;
+
 public class StepFirst {
     public static void main(String[] args) {
+        //StepFirst
+        System.out.println("Step first");
         int intValue = 21;
         float floatValue = 21.06f;
         boolean boolValue = true;
